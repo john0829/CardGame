3ed9e5130976a92f76972e014ac7546286e2145c
john <joh860829nny@gmail.com>
2021-06-08 22:51:32 +0800

    init structure v2

--- Player.h
+++ Player.h
@@ -11,31 +11,6 @@ class Player{
         int blood;
         int cardCount;
     public:
-        Player(string sourceFilePath):monsterList(nullptr), cardList(nullptr), blood(BLOOD_PLAYER), cardCount(0){
-            cout << blood << endl;
-            fstream file;
-            string line;
-            file.open(sourceFilePath, ios::in);
-            file >> cardCount;
-            file >> line;
-            if(!file){
-                cout << "file path is not found." << endl;
-                exit(1);
-            }
-
-            string type;
-            while(file >> type){
-                if(type== "monster"){
-                    string property;
-                    file >> property;
-                    MonsterCard* monster = new MonsterCard(file);
-                }
-                else{
-                    MagicCard* magic = new MagicCard(file);
-                }
-            }
-        }
-
-
+        Player(string);
 
 };
