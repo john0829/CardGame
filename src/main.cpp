0b0d2340686fb3dc978fb59c75c078eded876999
john <joh860829nny@gmail.com>
2021-06-09 10:08:11 +0800

    move input file location

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,13 @@
 #include "Config.h"
 
 int main(){
-    Player* p1 = new Player("p1.txt");
-    Player* p2 = new Player("p2.txt");
+    Player* p1 = new Player("./input/p1.txt");
+    Player* p2 = new Player("./input/p2.txt");
 
-    p1->readPlayCardSequence("seq1.txt");
-    p2->readPlayCardSequence("seq2.txt");
+    p1->readPlayCardSequence("./input/seq1.txt");
+    p2->readPlayCardSequence("./input/seq2.txt");
 
-    Rounds *rounds = new Rounds(p1, p2, "cards.txt");
+    Rounds *rounds = new Rounds(p1, p2, "./input/cards.txt");
     cout << "rounds start!" << endl;
     rounds->start();
     delete rounds;
